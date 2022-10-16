d1e7877a6c0e7358c01b94ff0274cfda35ad05cd
Melissa Vaiden <melissavaiden@gmail.com>
2022-10-16 17:55:29 -0400

    commit tree b48c0a1b04aae62579e6d58dd66e60f493897f4f

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        array = []
+        self.actors.map do |actor|
+            first = actor.first_name
+            last = actor.last_name
+            name = first + ' ' + last
+            array << name
+        end
+        array
+    end
 end
