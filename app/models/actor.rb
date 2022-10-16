2e6d439f0fff0adb267bae0a3fef15b41e213341
Melissa Vaiden <melissavaiden@gmail.com>
2022-10-16 17:55:42 -0400

    Completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        self.first_name + ' ' + self.last_name
+    end
+
+    def list_roles
+        array = []
+        self.characters.map do |char|
+            name = char.name
+            show = char.show.name
+            array << "#{name} - #{show}"
+        end
+        array
+    end
 end
